caa967b781535c964e6965e84f661f4b4eea605b
Yelyzaveta Poddubnaya <>
2020-11-04 13:13:17 +0200

    003_HW-> task1, task2 added

---
+++ 003_HW/task-1/timer.js
@@ -0,0 +1,57 @@
+﻿window.onload = function() {
+
+    let timer = document.getElementById("time"),
+        hours = document.getElementById("hours"),
+        minutes = document.getElementById("minutes"),
+        seconds = document.getElementById("seconds"),
+        defaultTime = new Date(),
+        timeCounter = function name() {
+            if(defaultTime.getHours() < 24){
+                defaultTime.setSeconds(defaultTime.getSeconds() + 1);
+
+                hours.innerHTML = (defaultTime.getHours() < 10 ? '0' : '') + defaultTime.getHours();
+                minutes.innerHTML = (defaultTime.getMinutes() < 10 ? '0' : '') + defaultTime.getMinutes();
+                seconds.innerHTML = (defaultTime.getSeconds() < 10 ? '0' : '') + defaultTime.getSeconds();
+            }else{
+                resetTimer();
+            }
+        },
+        counterInterval;
+
+        defaultTime.setHours(0, 0, 0, 0);
+
+    function startTimer() {
+        counterInterval = setInterval(timeCounter, 0.1);
+    }
+
+    function stopTimer() {
+        clearInterval(counterInterval);
+    }
+
+    function resetTimer() {
+        clearInterval(counterInterval);
+
+        defaultTime.setHours(0, 0, 0, 0);
+
+        hours.innerHTML = "00";
+        minutes.innerHTML = "00";
+        seconds.innerHTML = "00";
+    }
+
+    let startTrigger = document.getElementById("start"),
+        stopTrigger = document.getElementById("stop"),
+        resetTrigger = document.getElementById("reset");
+
+
+    startTrigger.addEventListener('click', () => {        
+        startTimer();
+    });
+
+    stopTrigger.addEventListener('click', () => {        
+        stopTimer();
+    });
+
+    resetTrigger.addEventListener('click', () => {        
+        resetTimer();
+    });
+}
---
+++ 003_HW/task-2/scripts.js
@@ -0,0 +1,28 @@
+﻿window.onload = function() {
+    let resultContainer = document.getElementById("result"),
+        resultVal = 0,
+        sum = function (){
+
+            let query = location.search.substring(1),
+                params = query.split("&");
+
+            for (let i = 0; i < params.length; i++) {
+
+                let pos = params[i].indexOf('=');
+                if (pos == -1) { 
+                    continue;
+                }
+
+                let argValue = Number(params[i].substring(pos + 1)); // Получение значения
+
+                if(typeof argValue === 'number' && !isNaN(argValue)){
+                    resultVal = resultVal + Number(argValue);
+                }
+
+            }
+
+            resultContainer.innerHTML = resultVal;
+        };
+
+        sum();
+}
