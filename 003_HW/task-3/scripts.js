62d1197b86760ab33298d79a57608722a50fd072
Yelyzaveta Poddubnaya <>
2020-11-04 16:01:45 +0200

    003_HW-> task3  added

--- 003_HW/task-3/scripts.js
+++ 003_HW/task-3/scripts.js
@@ -1,3 +1,33 @@
 ﻿window.onload = function() {
+    let resultContainer = document.getElementById("result");
+
+    let calculate = function (){
+        let pattern = /\d [\+\-\/\*]{1} \d/,
+            expression = prompt('Add an expression for the calculation using next format:\n"number +(or "-" or "*" or "/") number"'),
+            expTest = pattern.test(expression);
     
+        if(expTest){
+            let args = expression.split(" "),
+                operator = args[1],
+                resultVal;
+
+            if(operator === '+'){
+                resultVal = Number(args[0]) + Number(args[2]);
+            }else if(operator === '-'){
+                resultVal = Number(args[0]) - Number(args[2]);
+            }else if(operator === '*'){
+                resultVal = Number(args[0]) * Number(args[2]);
+            }else if(operator === '/'){
+                resultVal = Number(args[0]) / Number(args[2]);
+            }
+
+            resultContainer.innerHTML = resultVal;
+
+        }else{
+            resultContainer.innerHTML = 'Invalid expression format. Please, reload the page and try again.';
+        }
+    };
+
+    calculate();
+
 }
